4b9171b634aeaede7d15100b625e6108fb1b0e1b
moulasilz <qzarsixx@gmail.com>
2023-01-09 08:20:49 +0700

    up

---
+++ resources/views/public-profile/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./assets/include/bootstrap-5.2.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/include/bootstrap-5.2.3/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/include/fontawesome/css/all.min.css">
+    <link rel="stylesheet/less" type="text/css" href="./assets/css/styles.less">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/ico/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/ico/favicon-16x16.png">
+    <link rel="manifest" href="./assets/ico/site.webmanifest">
+    <title>Thông tin cá nhân</title>
+</head>
+
+<body>
+    <div class="d-flex justify-content-center">
+        <div class="phone rounded-2">
+            <div class="wrapper">
+                <div class="container-fluid heading">
+                    <div class="banner rounded-2">
+                        <img src="./assets/media/img/1500x500.jfif" alt="">
+                    </div>
+                    <div class="avatar d-flex justify-content-center">
+                        <div class="rounded-circle overflow-hidden d-flex align-items-center ">
+
+                            <img src="./assets/media/img/avt.jpg" width="100%" alt="">
+                        </div>
+                    </div>
+                    <div class="label-name d-flex flex-column align-items-center">
+                        <h3>Vi Thị Thu Thuỷ</h3>
+                        <p>freelancer</p>
+                    </div>
+                    <div class="contact-button d-flex justify-content-evenly">
+                        <a class="btn rounded border-0 bg-success text-white p-2" role="button" href="tel:0999999999"><i
+                                class="fa-solid fa-phone"></i><b>Contact</b></a>
+                        <a class="btn rounded border-0 bg-primary text-white p-2" role="button"
+                            href="https://facebook.com" target="_blank"><i
+                                class="fa-brands fa-facebook"></i><b>Facebook</b></a>
+                        <a class="btn rounded border-0 bg-warning text-white p-2" role="button"
+                            href="https://messenger.com" target="_blank"><i
+                                class="fa-brands fa-facebook-messenger"></i><b>Messenger</b></a>
+                    </div>
+
+                </div>
+                <div class="drop-line"></div>
+                <div class="container-fluid public-information">
+                    <ul>
+                        <li><i class="icon fa-solid fa-briefcase"></i><span><b>Freelancer</b></span></li>
+                        <li><i class="icon fa-solid fa-graduation-cap"></i><span>Studying at <b><a class="link-dark"
+                                        href="https://uneti.edu.vn/" target="_blank" rel="noopener noreferrer">Trường
+                                        Đại hoc Kinh tế - Kỹ thuật Công nghiệp (UNETI)</a></b></span></li>
+                        <li><i class="icon fa-solid fa-house-chimney"></i><span>Lives in <b>Hà Nội</b></span></li>
+                        <li><i class="icon fa-solid fa-location-dot"></i><span>From <b>Hà Nội</b></span></li>
+                        <li><i class="icon fa-brands fa-github"></i></i><span><b><a class="link-dark"
+                                        href="https://github.com/moulasilz" target="_blank"
+                                        rel="noopener noreferrer">Thanh Thuy</a></b></span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./assets/include/bootstrap-5.2.3/js/bootstrap.min.js"></script>
+    <script src="./assets/include/fontawesome/js/all.min.js"></script>
+    <script src="./assets/include/less.js-master/less.min.js"></script>
+</body>
+
+</html>
